0492d42441569da0e17ca8aae15af80b795226cf
Waldemar <34974+Velrok@users.noreply.github.com>
2024-05-25 00:38:18 +0100

    refactor

--- race.js
+++ race.js
@@ -1,82 +1,121 @@
 let backgroundImg;
-let racer1Img;
-let racer2Img;
-let racer3Img;
 
 const A_KEY = 65;
 const S_KEY = 83;
 const D_KEY = 68;
+const J_KEY = 74;
+const K_KEY = 75;
 
 const WIDTH = 1200;
 const HEIGHT = 900;
 
-let targetKey = A_KEY;
-
 let baseLevel = 650;
 
 let player = {
+  name: "player",
   x: 10,
   y: baseLevel - 100,
+  img: null,
+  speed: 35,
 };
 
 let oponent1 = {
+  name: "oponent1",
   x: 0,
   y: baseLevel - 100,
+  img: null,
+  speed: 1,
 };
 
 let oponent2 = {
+  name: "oponent2",
   x: 0,
   y: baseLevel,
+  img: null,
+  speed: 2,
 };
+
+let oponents = [oponent1, oponent2];
 
 function preload() {
   backgroundImg = loadImage("assets/desert.jpg");
-  racer1Img = loadImage("assets/shuttle.jpg");
-  racer2Img = loadImage("assets/drone.jpg");
-  racer3Img = loadImage("assets/anikin.jpg");
+  player.img = loadImage("assets/shuttle.jpg");
+  oponent1.img = loadImage("assets/drone.jpg");
+  oponent2.img = loadImage("assets/anikin.jpg");
 }
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
 }
 
-let winner = null;
+let winner = "unknown";
 
-function draw() {
-  oponent1.x += 1;
-  oponent2.x += 2;
+function drawImageEntity(imageEntity) {
+  image(imageEntity.img, imageEntity.x, imageEntity.y);
+}
 
-  if (!winner) {
+function drawWinner(winner) {
+  if (winner !== "unknown") {
+    textSize(100);
+    if (winner === "player") {
+      text("🏆 You win! 🏆", 300, HEIGHT / 2);
+    } else {
+      text("😞 Too slow. 😞", 300, HEIGHT / 2);
+    }
+  }
+}
+
+function drawOponents() {}
+
+function determineWinner() {
+  if (winner === "unknown") {
     if (player.x >= WIDTH) {
       console.log("player wins");
       winner = "player";
-    } else if (oponent1.x >= WIDTH) {
-      console.log("oponent1 wins");
-      winner = "oponent1";
-    } else if (oponent2.x >= WIDTH) {
-      console.log("oponent2 wins");
-      winner = "oponent2";
-    }
-  }
-
-  if (keyIsPressed) {
-    if (keyCode === targetKey) {
-      player.x += 35;
-      targetKey = targetKey === A_KEY ? S_KEY : A_KEY;
-    }
-  }
-
-  image(backgroundImg, 0, 0);
-  image(racer2Img, oponent1.x, oponent1.y);
-  image(racer3Img, oponent2.x, oponent2.y);
-  image(racer1Img, player.x, player.y);
-
-  if (winner) {
-    textSize(100);
-    if (winner === "player") {
-      text("🏆 You win 🏆", 300, HEIGHT / 2);
     } else {
-      text("😞 You lose 😞", 300, HEIGHT / 2);
+      oponents.forEach((oponent) => {
+        if (oponent.x >= WIDTH) {
+          console.log("oponent wins");
+          winner = oponent.name;
+        }
+      });
     }
   }
+}
+
+function drawScene() {
+  image(backgroundImg, 0, 0);
+  oponents.forEach((oponent) => {
+    drawImageEntity(oponent);
+  });
+  drawImageEntity(player);
+  drawWinner(winner);
+}
+
+function moveOponents() {
+  oponents.forEach((oponent) => {
+    oponent.x += oponent.speed;
+  });
+}
+
+const KEY_SET_1 = [A_KEY, J_KEY];
+const KEY_SET_2 = [S_KEY, K_KEY];
+let targetKeySet = KEY_SET_1;
+
+function movePlayer() {
+  if (keyIsPressed) {
+    console.log(keyCode);
+    if (targetKeySet.includes(keyCode)) {
+      player.x += player.speed;
+      targetKeySet = targetKeySet === KEY_SET_1 ? KEY_SET_2 : KEY_SET_1;
+    }
+  }
+}
+
+function draw() {
+  movePlayer();
+  moveOponents();
+  determineWinner();
+
+  drawScene();
 }
