83c7f3ffca2c238885828bd76b7a74bd98fa61e5
Waldemar <34974+Velrok@users.noreply.github.com>
2024-05-25 12:42:00 +0100

    add light speed

--- public/race.js
+++ public/race.js
@@ -1,15 +1,19 @@
 let backgroundImg;
+let lightSpeedImg;
 
 const A_KEY = 65;
 const S_KEY = 83;
 const D_KEY = 68;
 const J_KEY = 74;
 const K_KEY = 75;
+const F_KEY = 70;
 
 const WIDTH = 1200;
 const HEIGHT = 900;
 
 let baseLevel = 650;
+
+let areWeLightSpeed = false;
 
 let player = {
   name: "player",
@@ -38,6 +42,7 @@ let oponent2 = {
 let oponents = [oponent1, oponent2];
 
 function preload() {
+  lightSpeedImg = loadImage("/assets/lightspeed.jpg");
   backgroundImg = loadImage("assets/desert.jpg");
   player.img = loadImage("assets/shuttle.jpg");
   oponent1.img = loadImage("assets/drone.jpg");
@@ -46,6 +51,7 @@ function preload() {
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
+  frameRate(60);
 }
 
 let winner = "unknown";
@@ -88,6 +94,9 @@ function drawScene() {
   oponents.forEach((oponent) => {
     drawImageEntity(oponent);
   });
+  if (areWeLightSpeed) {
+    image(lightSpeedImg, 0, 0);
+  }
   drawImageEntity(player);
   drawWinner(winner);
 }
@@ -102,17 +111,58 @@ const KEY_SET_1 = [A_KEY, J_KEY];
 const KEY_SET_2 = [S_KEY, K_KEY];
 let targetKeySet = KEY_SET_1;
 
+class DataWindow {
+  constructor(size) {
+    this.size = size;
+    this.data = [];
+  }
+
+  addData(data) {
+    this.data.push(data);
+    if (this.data.length > this.size) {
+      this.data.shift();
+    }
+    return this.data;
+  }
+}
+
+const playerSpeedDataWindowSize = 2;
+let playerSpeedData = new DataWindow(playerSpeedDataWindowSize);
+
 function movePlayer() {
   if (keyIsPressed) {
-    console.log(keyCode);
     if (targetKeySet.includes(keyCode)) {
       player.x += player.speed;
+      playerSpeedData.addData(frameCount);
       targetKeySet = targetKeySet === KEY_SET_1 ? KEY_SET_2 : KEY_SET_1;
     }
   }
 }
 
+function keyTyped() {
+  // console.log(keyCode);
+  if (keyCode === F_KEY) {
+    fullscreen(!fullscreen());
+  }
+  return false;
+}
+
+const lightSpeedBarrier = 12;
+let lastMoveFrame = 0;
+
+function checkIfWeAreLightSpeed() {
+  if (playerSpeedData.data.length === playerSpeedDataWindowSize) {
+    let firstFrame = playerSpeedData.data[0];
+    let lastFrame = playerSpeedData.data[playerSpeedDataWindowSize - 1];
+    let frames = lastFrame - firstFrame;
+
+    areWeLightSpeed =
+      frames <= lightSpeedBarrier && lastFrame + 30 > frameCount;
+  }
+}
+
 function draw() {
+  checkIfWeAreLightSpeed();
   movePlayer();
   moveOponents();
   determineWinner();
